c9ccb946570140a996edc55add76a26cf23d69b2
Leandro Motta Barros <lmb@stackedboxes.org>
2024-09-26 11:07:40 -0300

    Add a header.Get() method
    This gives to access the headers we `Read()` from a response.

--- httpx/header_common.go
+++ httpx/header_common.go
@@ -45,6 +45,16 @@ func (h *header) Set(key, value string) {
 	h.SetCanonical(b2s(h.bufKV.key), value)
 }
 
+func (h *header) Get(key string) (string, bool) {
+	for _, kv := range h.h {
+		if strings.EqualFold(string(kv.key), key) {
+			return string(kv.value), true
+		}
+	}
+
+	return "", false
+}
+
 func (h *header) Add(key, value string) {
 	if h.setSpecialHeader(key, value) {
 		return
